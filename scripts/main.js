83a7467c8cd12a61a83616449659095a258cb481
Babina Banjara <offers682@gmail.com>
2022-01-18 08:52:52 +0545

    changed ui

--- scripts/main.js
+++ scripts/main.js
@@ -27,7 +27,7 @@ function generate_array()
         divs[i]=document.createElement("div");
         cont.appendChild(divs[i]);
         margin_size=0.1;
-        divs[i].style=" margin:0% " + margin_size + "%; background-color:blue; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
+        divs[i].style=" margin:0% " + margin_size + "%; background-color:#658E9C; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
     }
 }
 
